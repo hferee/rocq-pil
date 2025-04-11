c2b8346ccb38efd63b7e292b937a1c6ea5703d12
Hugo Férée <feree@irif.fr>
2025-04-11 14:44:58 +0200

    Experimental improvement for fastPitts

--- bin/fastPitts.ml
+++ bin/fastPitts.ml
@@ -1,121 +1,157 @@
 open PIL.Formulas
 open PIL.Extraction
+open Printer
 
 let top = Implies(Normal, Bot Normal, Bot Normal)
 let bot = Bot Normal
 
 
 (* tries to apply modus ponens for a variable and place the variable in the relevant stack *)
-let try_var_mp q (impVs, stack) f = match f with
+let try_var_mp q (imp_vars, stack) f = match f with
   | Implies(_, Var(_, q'), f') when q = q' ->
-      (impVs, f' :: stack)
-  | _ -> (f :: impVs, stack)
+      (imp_vars, f' :: stack)
+  | _ -> (f :: imp_vars, stack)
 
-  (* TODO : have new and old vars *)
+let conj a b = match a, b with
+| Bot k, _ | _, Bot k -> Bot k
+| Implies (_, Bot _, _) , x | x, Implies (_, Bot _, _) -> x
+| _ -> And(Normal, a, b) 
+let disj a b = match a, b with
+| Bot _, x | x , Bot _ -> x
+| Implies (k, Bot k', _) , _ | _, Implies (k, Bot k', _) -> Implies (k, Bot k', Bot k')
+| _ -> Or(Normal, a, b) 
 
-let conj a b = And(Normal, a, b) 
-let disj a b = Or(Normal, a, b) 
+let impl a b = match a, b with
+| Bot _, _ -> top
+| Implies (_, Bot _, _), _ -> b
+| _, Implies (_, Bot _, _) -> top
+| _ -> Implies(Normal, a, b) 
 
 let same_var q = function | Var (_, q') when q = q' -> true | _ -> false
 
-let rec e p vars impVs disjs nInv (stack : form list) : form =
+let rec list_compare c l1 l2 = match l1, l2 with
+| [], [] -> 0
+| [], _ :: _ -> -1
+| _ :: _, [] -> 1
+| h1 :: t1, h2 :: t2 -> if h1 < h2 then -1 else if h2 < h1 then 1 else list_compare c t1 t2
+
+let vars_order q r = match q, r with |Var(_, q), Var(_, r) -> list_compare (<) q r | _ -> 0
+
+let print_forms = List.iter (fun f -> print_string (string_of_formula f); print_string "; ")
+let add_var q context =
+  if List.exists (same_var q) context
+  then context
+  else (Var (Normal, q) :: context)
+
+(*
+  - vars : variables that have not been handled yet 
+  - context : variables in the context, that already occur in the interpolant
+  - imp_vars : implications variable 
+  - non_invs : non-inverstible rules 
+  - stack : formulas in the context that need to be handled *)
+let rec e p context vars imp_vars disjs non_invs (stack : form list) : form =
   match stack with
   | [] -> (match disjs with
           | (Or(_, x, y)) :: disjs' -> (* branching E3 *)
-              disj (e p vars impVs disjs' nInv [x]) (e p vars impVs disjs' nInv [y])
+              disj (e p context vars imp_vars disjs' non_invs [x]) (e p context vars imp_vars disjs' non_invs [y])
           | _ -> (* no more disjunctions ; need to handle invertible *)
-             conj (List.fold_left (fun acc f -> if same_var p f then acc else conj acc f) top vars) (* E1 *)
-                  (conj (List.fold_left (e8 p vars impVs disjs nInv) top nInv)(* E8 *)
-                        (List.fold_left (e4 p vars impVs disjs nInv) top impVs)) (* E4 *)
+             conj (List.fold_left (fun acc f -> if same_var p f then acc else conj acc f) top (List.sort vars_order vars)) (* E1 *)
+                  (conj (List.fold_left (e8 p context [] imp_vars disjs non_invs) top non_invs)(* E8 *)
+                        (List.fold_left (e4 p context [] imp_vars disjs non_invs) top imp_vars)) (* E4 *)
         ) 
   | Bot k :: _s -> Bot k;
-  | And(_, x, y) :: s -> e p vars impVs disjs nInv (x :: y :: s)
-  | Or (k, x, y) :: s -> e p vars impVs (Or(k, x, y) :: disjs) nInv s
+  | And(_, x, y) :: s -> e p context vars imp_vars disjs non_invs (x :: y :: s)
+  | Or (k, x, y) :: s -> e p context vars imp_vars (Or(k, x, y) :: disjs) non_invs s
   | Implies (k, Var (k', q), c) :: s ->
-      if List.exists (same_var q) vars 
-      then e p vars impVs disjs nInv (c :: s) (* E5' *)
-      else e p vars (Implies (k, Var (k', q), c) :: impVs) disjs nInv s
-  | Implies (_, Bot _', _) :: s -> e p vars impVs disjs nInv s (* custom *)
-  | Implies(k', And(k, x, y), z) :: s ->
-      e p vars impVs disjs nInv (Implies(k', x, Implies(k, y, z)) :: s) (* E6 *)
-  | Implies(k, Or(_, x, y), z) :: s ->
-      e p vars impVs disjs nInv (Implies(k, x, z) :: Implies(k, y, z) :: s) (* E7 *)
+      if List.exists (same_var q) context
+      then e p context vars imp_vars disjs non_invs (c :: s) (* E5' *)
+      else e p context vars (Implies (k, Var (k', q), c) :: imp_vars) disjs non_invs s
+  | Implies (_, Bot _', _) :: s -> e p context vars imp_vars disjs non_invs s (* custom *)
+  | Implies(_, And(_, x, y), z) :: s ->
+      e p context vars imp_vars disjs non_invs (impl x (impl y z) :: s) (* E6 *)
+  | Implies(_, Or(_, x, y), z) :: s ->
+      e p context vars imp_vars disjs non_invs (impl x z :: impl y z :: s) (* E7 *)
+  | Implies(_, Implies(_, Bot _, _), Bot _) :: _ -> bot (* custom*)
   | Implies(k, Implies(k', x, y), z) :: s ->
-      e p vars impVs disjs (Implies(k, Implies(k', x, y), z) :: nInv) s
-  | Var(k, q) :: s ->
-    let (impVs', s') = List.fold_left (try_var_mp q) ([], s) impVs in
-        e p (Var(k, q) :: vars) impVs' disjs nInv s'
+      e p context vars imp_vars disjs (Implies(k, Implies(k', x, y), z) :: non_invs) s
+  | Var(_, q) :: s ->
+    let (imp_vars', s') = List.fold_left (try_var_mp q) ([], s) imp_vars in
+        e p (add_var q context) (add_var q vars) imp_vars' disjs non_invs s'
   | Box _ :: _ | Implies(_, Box _, _) :: _ -> failwith "Modalities not handled yet" 
-and e8 p vars impVs disjs nInv acc f = match f with
-| Implies (k, Implies(k', x, y), z) ->
-  let nInv' = List.filter ((<>) f) nInv in
-  conj acc (Implies (k, Implies(k', e p vars impVs disjs nInv' [x; Implies(k', y, z)],
-                                    a p vars impVs disjs nInv' [x; Implies(k', y, z)] y),
-                        e p vars impVs disjs nInv' [z]))
+and e8 p context vars imp_vars disjs non_invs acc f = match f with
+| Implies (_, Implies(_, x, y), z) ->
+  let non_invs' = List.filter ((<>) f) non_invs in
+  conj acc (impl (impl (e p context vars imp_vars disjs non_invs' [x; impl y z])
+                       (a p context imp_vars disjs non_invs' [x; impl y z] y))
+                 (e p context vars imp_vars disjs non_invs' [z]))
 | _ -> failwith "Unexpected non-invertible rule (E8)"
-and e4 p vars impVs disjs nInv acc f = match f with
-| Implies (k, Var(k', q), x) when p <> q ->
-  let impVs' = List.filter ((<>) f) impVs in
-  conj acc (Implies (k, Var(k', q), e p vars impVs' disjs nInv [x]))
+and e4 p context vars imp_vars disjs non_invs acc f = match f with
+| Implies (_, Var(k', q), x) when p <> q ->
+  (* TODO: we made sure this never happens ; but still checking for debug*)
+  (* if List.exists (same_var q) context then failwith "redundant variable E4" else is that an issue ? TODO *)
+  let imp_vars' = List.filter ((<>) f) imp_vars in
+  conj acc (impl (Var(k', q)) (e p (add_var q context) vars imp_vars' disjs non_invs [x]))
 | Implies (_, Var(_, _), _) -> acc
 | _ -> failwith "Unexpected non-invertible rule (E4)"
-and a p vars impVs disjs nInv (stack : form list) (rhs : form) : form =
+and a p context imp_vars disjs non_invs (stack : form list) (rhs : form) : form =
   match stack with
   | [] ->
     (match disjs with
       | Or(k, x, y) :: disjs' -> (* branching A3 *)
-          disj (Implies(k, e p vars impVs disjs' nInv [x], a p vars impVs disjs' nInv [x] rhs))
-               (Implies(k, e p vars impVs disjs' nInv [y], a p vars impVs disjs' nInv [y] rhs))
+          conj (Implies(k, e p context [] imp_vars disjs' non_invs [x], a p context imp_vars disjs' non_invs [x] rhs))
+               (Implies(k, e p context [] imp_vars disjs' non_invs [y], a p context imp_vars disjs' non_invs [y] rhs))
       | _ -> (* no more disjunctions *)
               match rhs with
-              | Var (_, q) when p = q && List.exists (same_var p) vars -> top (* A10  *)
-              | And (k, x, y) -> And(k, a p vars impVs disjs nInv [] x,
-                                        a p vars impVs disjs nInv [] y) (* A11 *)
-              | Implies (k, x, y) -> Implies(k, e p vars impVs disjs nInv [x],
-                                                a p vars impVs disjs nInv [x] y) (* A13 *)
+              | Var (_, q) when p = q && List.exists (same_var p) context -> top (* A10  *)
+              | Var (k, q) when p <> q -> Var(k, q) (* Experimental *)
+              | And (_, x, y) -> conj (a p context imp_vars disjs non_invs [] x)
+                                      (a p context imp_vars disjs non_invs [] y) (* A11 *)
+              | Implies (_, x, y) -> impl(e p context [] imp_vars disjs non_invs [x])
+                                         (a p context imp_vars disjs non_invs [x] y) (* A13 *)
               | _ ->
                 disj ( disj
-                        (List.fold_left (a8 p vars impVs disjs nInv rhs) bot nInv)(* A8 *)
-                        (List.fold_left (a4 p vars impVs disjs nInv rhs) bot impVs)) (* A4 *)
+                        (List.fold_left (a8 p context imp_vars disjs non_invs rhs) bot non_invs)(* A8 *)
+                        (List.fold_left (a4 p context imp_vars disjs non_invs rhs) bot imp_vars)) (* A4 *)
                      (match rhs with
-                     | Var(k, q) when q <> p -> Var(k, q) (* A9 *)
-                     | Or(k, x, y) -> Or(k, a p vars impVs disjs nInv [] x, (* A12 *)
-                                            a p vars impVs disjs nInv [] y)
+                     | Var(k, q) when q <> p -> Var(k, q) (* A9 : Experimental : should never happen *)
+                     | Or(_, x, y) -> disj (a p context imp_vars disjs non_invs [] x) (* A12 *)
+                                           (a p context imp_vars disjs non_invs [] y)
                     | _ -> bot
                      )
     )
-  | And(_, x, y) :: s -> a p vars impVs disjs nInv (x :: y :: s) rhs (* A2 *)
+  | And(_, x, y) :: s -> a p context imp_vars disjs non_invs (x :: y :: s) rhs (* A2 *)
   | Implies (k, Var (k', q), c) :: s ->
-    if List.exists (same_var q) vars
-    then a p vars impVs disjs nInv (c :: s) rhs (* A5' *)
-    else a p vars (Implies (k, Var (k', q), c) :: impVs) disjs nInv s rhs
-  | Implies (_, Bot _', _) :: s -> a p vars impVs disjs nInv s rhs (* custom *)
-  | Implies(k', And(k, x, y), z) :: s ->
-    a p vars impVs disjs nInv (Implies(k', x, Implies(k, y, z)) :: s) rhs (* E6 *)
-  | Implies(k, Or(_, x, y), z) :: s ->
-    a p vars impVs disjs nInv (Implies(k, x, z) :: Implies(k, y, z) :: s) rhs (* E7 *)
+    if List.exists (same_var q) context
+    then a p context imp_vars disjs non_invs (c :: s) rhs (* A5' *)
+    else a p context (Implies (k, Var (k', q), c) :: imp_vars) disjs non_invs s rhs
+  | Implies (_, Bot _', _) :: s -> a p context imp_vars disjs non_invs s rhs (* custom *)
+  | Implies(_, And(_, x, y), z) :: s ->
+    a p context imp_vars disjs non_invs (impl x (impl y z) :: s) rhs (* E6 *)
+  | Implies(_, Or(_, x, y), z) :: s ->
+    a p context imp_vars disjs non_invs (impl x z :: impl y z :: s) rhs (* E7 *)
   | Implies(k, Implies(k', x, y), z) :: s ->
-      a p vars impVs disjs (Implies(k, Implies(k', x, y), z) :: nInv) s rhs
-  | Var(k, q) :: s -> if q = p && same_var p rhs then top else (* A10 *)
-    let (impVs', s') = List.fold_left (try_var_mp q) ([], s) impVs in
-        a p (Var(k, q) :: vars) impVs' disjs nInv s' rhs
-  | Bot _ :: s -> a p vars impVs disjs nInv s rhs
-  | Or (k, x, y) :: s -> a p vars impVs (Or(k, x, y) :: disjs) nInv s rhs
+      a p context imp_vars disjs (Implies(k, Implies(k', x, y), z) :: non_invs) s rhs
+  | Var(_, q) :: s -> if q = p && same_var p rhs then top else (* A10 *)
+    let (imp_vars', s') = List.fold_left (try_var_mp q) ([], s) imp_vars in
+        a p (add_var q context) imp_vars' disjs non_invs s' rhs
+  | Bot _ :: s -> a p context imp_vars disjs non_invs s rhs
+  | Or (k, x, y) :: s -> a p context imp_vars (Or(k, x, y) :: disjs) non_invs s rhs
   | Box _ :: _ | Implies(_, Box _, _) :: _ -> failwith "Modalities not handled yet" 
-and a8 p vars impVs disjs nInv rhs acc f = match f with
-  | Implies (k, Implies(k', x, y), z) ->
-    let nInv' = List.filter ((<>) f) nInv in
-    disj acc (And (k, Implies(k', e p vars impVs disjs nInv' [x; Implies(k', y, z)],
-                                  a p vars impVs disjs nInv' [x; Implies(k', y, z)] y),
-                          a p vars impVs disjs nInv' [z] rhs))
+and a8 p context imp_vars disjs non_invs rhs acc f = match f with
+  | Implies (_, Implies(_, x, y), z) ->
+    let non_invs' = List.filter ((<>) f) non_invs in
+    disj acc (conj (impl (e p context [] imp_vars disjs non_invs' [x; impl y z])
+                          (a p context imp_vars disjs non_invs' [x; impl y z] y))
+                  (a p context imp_vars disjs non_invs' [z] rhs))
   | _ -> failwith "Unexpected non-invertible rule (A8)"
-and a4 p vars impVs disjs nInv rhs acc f = match f with
-  | Implies (k, Var(k', q), x) when p <> q ->
-    let impVs' = List.filter ((<>) f) impVs in
-    disj acc (And (k, Var(k', q), a p vars impVs' disjs nInv [x] rhs))
+and a4 p context imp_vars disjs non_invs rhs acc f = match f with
+  | Implies (_, Var(k', q), x) when p <> q ->
+    if List.exists (same_var q) context then
+      a p context imp_vars disjs non_invs [x] rhs
+    else
+    let imp_vars' = List.filter ((<>) f) imp_vars in
+    disj acc (conj (Var(k', q)) (a p (Var(k', q) :: context) imp_vars' disjs non_invs [x] rhs))
     (* TODO: could factor out q for all q → X *)
   | Implies (_, Var(_,_), _) -> acc
   | _ -> failwith "Unexpected non-invertible rule (A4)"
-(* invariant : vars and impVs can never produce instances of ImpLVar *)
-
-(* todo actually use p! -> check E *)
+(* invariant : vars and imp_vars can never produce instances of ImpLVar *)
--- bin/test_fast.ml
+++ bin/test_fast.ml
@@ -13,7 +13,7 @@ open PIL.DecisionProcedure
 (* n first variables *)
 let rec n_vars n = if n <= 1 then [['p']] else [Char.chr ((Char.code 'p') + n)] :: n_vars (n - 1)
 
-let e_vars = from_list ~name:"variables" (n_vars 4)
+let e_vars = from_list ~name:"variables" (n_vars 2)
 
 (* Type term is recursive, hence we need a lazy enumeration first. *)
 let rec l_e_formulas = lazy
@@ -22,8 +22,8 @@ let rec l_e_formulas = lazy
       union
 	[ single (Bot Modal) ;
     map e_vars (fun x -> Var (Modal, x)) ;
-	(*  map (pair r_formulas r_formulas) (fun (t1, t2) -> And (Modal, t1, t2)) ;
-    map (pair r_formulas r_formulas) (fun (t1, t2) -> Or (Modal, t1, t2)) ; *)
+	  map (pair r_formulas r_formulas) (fun (t1, t2) -> And (Modal, t1, t2)) ;
+    map (pair r_formulas r_formulas) (fun (t1, t2) -> Or (Modal, t1, t2)) ;
    map (pair r_formulas r_formulas) (fun (t1, t2) -> Implies (Modal, t1, t2)) ;
    (* map r_formulas (fun x -> Box x); Don't test modal formulas for now *)
 	]
@@ -47,14 +47,30 @@ let usage_string =
 let v : variable = coqstring_of_camlstring "p"
 
 let show_test (f: form) : string =
-  let fa = isl_simplified_A v f
-  and fast_fa = (a v [] [] [] [] [] f) in
-  let fe = isl_simplified_E v f
-  and fast_fe = (e v [] [] [] [] [f]) in
+(*   let _ = print_string "computing fast_fa" in
+ *)  let fast_fa = isl_simp(a v [] [] [] [] [] f) in
+(*   let _ = print_string "computing fa" ; flush stdout in
+ *)  let fa = isl_simplified_A v f in
+(*   let _ = print_string "computing fe" in
+ *)  let fe = isl_simplified_E v f in
+(*   let _ = print_string "computing fast_fe" in
+ *)  let fast_fe =  isl_simp(e v [] [] [] [] [] [f]) in
+(*   let _ = print_string "checking" in
+*)
+(*
+ let sfa = string_of_formula fa
+ and sfe = string_of_formula fe
+ and sfast_fa = string_of_formula fast_fa
+ and sfast_fe = string_of_formula fast_fe in
+ if weight Normal fa = weight Normal fast_fa && weight Normal fe = weight Normal fast_fe then "OK" else
+  ("Difference with " ^ string_of_formula f ^ ":\n A:\n" ^
+  sfa ^ "\nFast A:\n" ^ sfast_fa ^ "\n" ^
+  "E: \n" ^ sfe ^ "\nFast E:\n" ^ sfast_fe ^ "\n")
+  *)
   let equiva = And(Modal, Implies(Modal, fa, fast_fa), Implies(Modal, fast_fa, fa)) in
   let equive = And(Modal, Implies(Modal, fe, fast_fe), Implies(Modal, fast_fe, fe)) in
   match coq_Provable_dec Modal [] equiva, coq_Provable_dec Modal [] equive with
-  | Coq_inl _,  Coq_inl _ -> "" | _ -> (
+  | Coq_inl _,  Coq_inl _ -> "OK" | _ -> (
     "Error with " ^ string_of_formula f ^ ":\n A:\n" ^
     string_of_formula fa ^ "\nFast A:\n" ^ string_of_formula fast_fa ^ "\n" ^
     "E: \n" ^ string_of_formula fe ^ "\nFast E:\n" ^ string_of_formula fast_fe ^ "\n")
